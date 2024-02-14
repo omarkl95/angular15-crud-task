66c6179478989436279a72acac4cef0e41004e98
omarkl95 <omarkamal521@gmail.com>
2024-02-14 15:10:02 +0200

    ddf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthComponent } from './auth/auth.component';
+import { CrudListComponent } from './crud-list/crud-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    CrudListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/crud-list/crud-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-list',
+  templateUrl: './crud-list.component.html',
+  styleUrls: ['./crud-list.component.scss']
+})
+export class CrudListComponent {
+
+}
